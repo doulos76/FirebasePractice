2ad32c9c266f646f9e57fad8574bac0060f47495
doulos76 <inojyes@gmail.com>
2019-06-01 23:45:49 +0900

    add observe single event

--- FirebasePractice/FirebasePractice/ViewController.swift
+++ FirebasePractice/FirebasePractice/ViewController.swift
@@ -7,16 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
   
+  // MARK:- Properties
+  
   @IBOutlet weak var dataDisplayLabel: UILabel!
+  
+  // MARK:- View Life Cycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    updateLabel()
   }
 
+  // MARK:- private method
+  
+  fileprivate func updateLabel() {
+    var ref: DatabaseReference!
+    ref = Database.database().reference()
+
+    // observeSingleEvent(of: T##DataEventType, with: T##(DataSnapshot) -> Void)
+    ref.child("test").observeSingleEvent(of: .value) { (snapshot) in
+      let value = snapshot.value as? String
+
+      DispatchQueue.main.async {
+        self.dataDisplayLabel.text = value
+      }
+    }
+    
+    // observe(T##eventType: DataEventType##DataEventType, with: T##(DataSnapshot) -> Void)
+    ref.child("test").observe(.value) { (snapshot) in
+      let value = snapshot.value as? String
+      
+      DispatchQueue.main.async {
+        self.dataDisplayLabel.text = value
+      }
+
+    }
+  }
 
 }
 
